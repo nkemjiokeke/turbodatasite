3ba8616f2f23bef47e200a0c12b4d7a85fdb30bd
Nkemjika Okeke <okekenkemjika14@gmail.com>
2026-06-05 10:19:06 -0400

    new updated enroll page

---
+++ app/enrol/page.tsx
@@ -0,0 +1,12 @@
+export default function EnrolPage() {
+  return (
+    <html>
+      <body style={{ margin: 0, padding: 0 }}>
+        <iframe
+          src="/enrol.html"
+          style={{ width: '100%', height: '100vh', border: 'none' }}
+        />
+      </body>
+    </html>
+  )
+}
